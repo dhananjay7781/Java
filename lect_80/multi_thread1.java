1720cc9e3acaa50da0c46b5598121600ececd0a5
Dhananjay Patil <89823947+dhananjay7781@users.noreply.github.com>
2022-03-22 18:56:11 +0530

    Delete lect_80 directory

--- lect_80/multi_thread1.java
+++
@@ -1,7 +0,0 @@
-class threaddemo{
-	public static void main(String [] args) {
-		System.out.println("MultiThreading");
-		String threadName = Thread.currentThread().getName();
-		System.out.println("Thread Name = " + threadName);
-	}
-}
